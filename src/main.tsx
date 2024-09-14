b460d92b5f22c91dfa409d3bb0a597d1e6881c33
kevin <kevinli8831@gmail.com>
2024-09-15 04:27:51 +0800

    set basename

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
-import Header from './components/Header.tsx';
-import Home from './pages/Home.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+import Header from "./components/Header.tsx";
+import Home from "./pages/Home.tsx";
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Header />,
-    errorElement: <div>Error Page!!!!!</div>,
-    children: [
-      { index: true, element: <Navigate to="/marksix/home" replace /> },
-      {
-        path: '/marksix/home',
-        element: <Home />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Header />,
+      errorElement: <div>Error Page!!!!!</div>,
+      children: [
+        { index: true, element: <Navigate to="/marksix/home" replace /> },
+        {
+          path: "/marksix/home",
+          element: <Home />,
+        },
+      ],
+    },
+  ],
+  { basename: import.meta.env.BASE_URL },
+);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
-)
+);
